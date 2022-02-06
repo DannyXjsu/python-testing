2e171867fa2a34d1b2b7eee46d094852557f5823
DannyXjsu <daniel.nazar.br@gmail.com>
2022-02-06 02:53:13 -0300

    Major character-based changes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import glob
+import sys
 import time
 
 import enquiries
@@ -12,11 +13,13 @@ import fileman
 # These are temporary, they are only used to parse the information to the game class, the reason why I made it this way
 # it's because it's easier to do it on the top of the file, and it's more readable with how classes look like
 game_name = "RPG-Test"
-game_devel_mode = False
+game_devel_mode = True
 game_ver = 0.2
 game_ver_stage = 'alpha'
 game_file_name_ext = os.path.basename(__file__)
 game_file_name = game_file_name_ext[:-3]
+game_autosave = False
+game_character_selected = False
 
 # Developer versions
 if game_devel_mode:
@@ -45,25 +48,28 @@ class Game:  # Game class, might seem weird doing a game class, but I think it m
 
     def clear_data(self):  # Deletes everything in the save folder along with the folder itself and deletes settings.cfg
         if os.path.exists("save"):
-            save_files = glob.glob('save/*')
+            save_files = glob.glob('save/*.sav')
             for f in save_files:
                 try:
                     os.remove(f)
                 except OSError as error:
                     print("Error: %s : %s" % (f, error.strerror))
             os.rmdir("save")
-
+        if os.path.exists('characters/'):
+            character_files = glob.glob("characters/*.chr")
+            for f in character_files:
+                try:
+                    os.remove(f)
+                except OSError as error:
+                    print("Error: %s : %s" % (f, error.strerror))
         if os.path.exists("settings.cfg"):
             os.remove("settings.cfg")
 
-    def save(self, save_name, player_name, player_gender, player_age):  # Saves the game, in the future this will be
+    def save(self, save_name):  # Saves the game, in the future this will be
         # extremely huge
-        savePath = 'save/{}.sav'.format(save_name)  # Sets the save path to a name parsed into the function
-        with open(savePath, 'w') as saveFile:
-            saveFile.write('pName={}\n'.format(player_name))
-            saveFile.write('pGender={}\n'.format(player_gender))
-            saveFile.write('pAge={}\n'.format(player_age))
-            saveFile.close()
+        char = glob.glob('characters/*.chr')
+        fileman.write('save/', save_name, 'sav', 'CharFile={}\n'.format(char[int(player.char_index)]))
+        print('Saved to save/{}.sav'.format(save_name))
 
     # This is still in testing, might be extremely broken, in the future I might completely change this
     def load(self, save_name):
@@ -94,15 +100,19 @@ del game_file_name_ext
 
 
 class Player:
-    def __init__(self, name, gender, age, oldfolk):
+    def __init__(self, name, gender, age, oldfolk, char_index):
         self.name = name
         self.gender = gender
         self.age = age
         self.oldfolk = oldfolk
+        self.char_index = char_index
 
     def print_info(self):
         game.print_simple_line()
-        print('Name: ', self.name, '|', 'Age: ', self.age, '|', 'Gender: ', self.gender)
+        if game_devel_mode:
+            print('Name: ', self.name, '|', 'Age: ', self.age, '|', 'Gender: ', self.gender, '| DEV-Index: ', self.char_index)
+        else:
+            print('Name: ', self.name, '|', 'Age: ', self.age, '|', 'Gender: ', self.gender)
         game.print_simple_line()
 
     def set_name(self, first_time):
@@ -163,36 +173,125 @@ class Player:
 
         del ageVerifier
 
+    def delete(self):
+        clear()
+        characters = glob.glob('characters/*.chr')
+        try:
+            c_choice = enquiries.choose('Delete Character', characters)
+
+            if c_choice == characters[0]:
+                if enquiries.confirm('Are you sure you want to delete this character?'):
+                    os.remove(characters[0])
+            elif c_choice == characters[1]:
+                if enquiries.confirm('Are you sure you want to delete this character?'):
+                    os.remove(characters[1])
+            elif c_choice == characters[2]:
+                if enquiries.confirm('Are you sure you want to delete this character?'):
+                    os.remove(characters[2])
+            elif c_choice == characters[3]:
+                if enquiries.confirm('Are you sure you want to delete this character?'):
+                    os.remove(characters[3])
+            elif c_choice == characters[4]:
+                if enquiries.confirm('Are you sure you want to delete this character?'):
+                    os.remove(characters[4])
+
+        except ValueError:
+            clear()
+            print('No characters found, or an error occurred.')
+            input('Press Enter to continue...')
+
+    def select(self):
+        global game_character_selected
+        clear()
+        print('Character currently selected: {}'.format(player.name))
+        characters = glob.glob('characters/*.chr')
+        try:
+            c_choice = enquiries.choose('Select Character', characters)
+
+            if c_choice == characters[0]:
+                self.char_index = fileman.get_definition(characters[0], 'pCharIndex', '=')
+                self.name = fileman.get_definition(characters[0], 'pName', '=')
+                self.gender = fileman.get_definition(characters[0], 'pGender', '=')
+                self.age = fileman.get_definition(characters[0], 'pAge', '=')
+                game_character_selected = True
+            elif c_choice == characters[1]:
+                self.char_index = fileman.get_definition(characters[1], 'pCharIndex', '=')
+                self.name = fileman.get_definition(characters[1], 'pName', '=')
+                self.gender = fileman.get_definition(characters[1], 'pGender', '=')
+                self.age = fileman.get_definition(characters[1], 'pAge', '=')
+                game_character_selected = True
+            elif c_choice == characters[2]:
+                self.char_index = fileman.get_definition(characters[2], 'pCharIndex', '=')
+                self.name = fileman.get_definition(characters[2], 'pName', '=')
+                self.gender = fileman.get_definition(characters[2], 'pGender', '=')
+                self.age = fileman.get_definition(characters[2], 'pAge', '=')
+                game_character_selected = True
+            elif c_choice == characters[3]:
+                self.char_index = fileman.get_definition(characters[3], 'pCharIndex', '=')
+                self.name = fileman.get_definition(characters[3], 'pName', '=')
+                self.gender = fileman.get_definition(characters[3], 'pGender', '=')
+                self.age = fileman.get_definition(characters[3], 'pAge', '=')
+                game_character_selected = True
+            elif c_choice == characters[4]:
+                self.char_index = fileman.get_definition(characters[4], 'pCharIndex', '=')
+                self.name = fileman.get_definition(characters[4], 'pName', '=')
+                self.gender = fileman.get_definition(characters[4], 'pGender', '=')
+                self.age = fileman.get_definition(characters[4], 'pAge', '=')
+                game_character_selected = True
+        except ValueError:
+            clear()
+            print('No characters found, create one first.')
+            input('Press Enter to continue...')
+
+    def save(self):
+        if not os.path.exists('characters/'):
+            os.makedirs('characters/')
+        self.char_index = len(glob.glob('characters/*.chr'))
+        fileman.write('characters/', self.name.lower(), 'chr',
+                      'pCharIndex={}\n'
+                      'pName={}\n'
+                      'pGender={}\n'
+                      'pAge={}\n'.format(self.char_index, self.name, self.gender, self.age)
+                      )
+
     def create(self, set_name_first_time):
         clear()
-        creationLoop = True
-        self.set_name(set_name_first_time)
-        self.set_gender()
-        self.set_age()
+        game_characters = len(glob.glob('characters/*.chr'))
+        if game_characters == 5:
+            print('Maximum characters reached, delete a few to create more.')
+            input('Press Enter to continue...')
+        else:
+            print('Total Characters: ', game_characters, '/5')
+            creationLoop = True
+            self.set_name(set_name_first_time)
+            self.set_gender()
+            self.set_age()
+            clear()
+            while creationLoop:
+                print('Total Characters: ', game_characters, '/5')
+                print('Character Creation Overview:')
+                game.print_simple_line()
+                print('Name: {}'.format(self.name))
+                print('Age: {}'.format(self.age))
+                print('Gender: {}'.format(self.gender))
 
-        while creationLoop:
-            print('Character Creation Overview:')
-            game.print_simple_line()
-            print('Name: {}'.format(self.name))
-            print('Age: {}'.format(self.age))
-            print('Gender: {}'.format(self.gender))
-
-            if enquiries.confirm('Is everything in order?'):
-                creationLoop = False
-            else:
-                change_options = ['Name', 'Age', 'Gender', 'Everything is in order']
-                change_selected = enquiries.choose('What do you want to change?', change_options)
-                clear()
-                if change_selected == 'Name':
-                    self.set_name(0)
-                elif change_selected == 'Age':
-                    self.set_age()
-                elif change_selected == 'Gender':
-                    self.set_gender()
-                else:
+                if enquiries.confirm('Is everything in order?'):
                     creationLoop = False
-        del creationLoop
-
+                else:
+                    change_options = ['Name', 'Age', 'Gender', 'Everything is in order']
+                    change_selected = enquiries.choose('What do you want to change?', change_options)
+                    clear()
+                    if change_selected == 'Name':
+                        self.set_name(0)
+                    elif change_selected == 'Age':
+                        self.set_age()
+                    elif change_selected == 'Gender':
+                        self.set_gender()
+                    else:
+                        creationLoop = False
+            del creationLoop
+            self.save()
+            game_characters += 1
 
 def clear():
     if os.system.__name__ == 'nt':
@@ -201,11 +300,10 @@ def clear():
         os.system('clear')
 
 
-player = Player("", "", 0, 0)
+player = Player("", "", 0, 0, 0)
 
 
 def boot():  # Starts up the essential things, sort of like a computer booting
-
     clear()
 
     print(game.info_format)  # Prints out the game info
@@ -217,8 +315,6 @@ def boot():  # Starts up the essential things, sort of like a computer booting
 
     if not os.path.exists(settingsPath):  # A way to know if it's the first time that the user is running the program
 
-        player.create(1)
-
         with open(settingsPath, 'w') as settingsFile:  # Creates settings file since it doesn't exist
             settingsFile.write('# This file was automatically generated by , if file is erased, a full reset '
                                'will be done')
@@ -227,10 +323,11 @@ def boot():  # Starts up the essential things, sort of like a computer booting
             os.makedirs('save/')
         except FileExistsError:
             pass
-
-        game.save("auto", player.name, player.gender, player.age)
+        if game_autosave:
+            game.save("auto", player.name, player.gender, player.age)
     else:
-        game.load("auto")
+        if game_autosave:
+            game.load("auto")
 
 
 def menu_save_load():
@@ -242,46 +339,89 @@ def menu_save_load():
 
         if msl_choice == 'Save Game':
             print('Saving...')
-            game.save("auto", player.name, player.gender, player.age)
-            clear()
+            game.save(player.name.lower(),)
             print('Saved.')
             time.sleep(1)
         elif msl_choice == 'Load Game':
             print('Loading...')
-            game.load('auto')
-            clear()
+            game.load(player.name.lower())
             print('Loaded.')
             time.sleep(1)
         else:
             break
 
 
-def menu_options():
-    clear()
-    mo_options = ['Reset Game (This will delete all of your data and will restart the game)', 'Back to Main Menu']
-    mo_choice = enquiries.choose('What do you want to do?', mo_options)
-
-    if mo_choice == 'Reset Game (This will delete all of your data and will restart the game)':
-        game.clear_data()
+def menu_cheat():
+    while True:
         clear()
-        boot()
-    else:
-        pass
+        mc_options = ['Player Cheats', 'Back']
+        mc_choice = enquiries.choose('Cheat Menu', mc_options)
+
+        if mc_choice == 'Player Cheats':
+            clear()
+            while True:
+                mc_options = ['Change Name', 'Change Gender', 'Change Age', 'Back']
+                mc_choice = enquiries.choose('Player Cheats', mc_options)
+                if mc_choice == 'Change Name':
+                    player.set_name(0)
+                elif mc_choice == 'Change Gender':
+                    player.set_gender()
+                elif mc_choice == 'Change Age':
+                    player.set_age()
+                else:
+                    break
+        else:
+            break
+
+
+def menu_options():
+    while True:
+        clear()
+        if not game_devel_mode:
+            mo_options = ['Reset Game (This will delete all of your data and will restart the game)',
+                          'Back to Main Menu']
+        else:
+            mo_options = ['Reset Game (This will delete all of your data and will restart the game)',
+                          'Cheat Menu',
+                          'Back to main menu']
+        mo_choice = enquiries.choose('What do you want to do?', mo_options)
+
+        if mo_choice == 'Reset Game (This will delete all of your data and will restart the game)':
+            game.clear_data()
+            clear()
+            os.execv(sys.executable, ['python'] + sys.argv)
+        elif mo_choice == 'Cheat Menu':
+            menu_cheat()
+        else:
+            break
 
 
 def main():
     while True:
-        print('Hello World')
-        print(game.file_name)
-
-        player.print_info()
-        mm_options = ['Save/Load', 'Options', 'Exit']
+        if game_devel_mode:
+            print('Development Mode')
+        if not game_character_selected:
+            print('Select a Character')
+        else:
+            player.print_info()
+        mm_options = ['Select Character', 'Create Character', 'Delete Character', 'Save/Load', 'Options', 'Exit']
         mm_choice = enquiries.choose('Main Menu ', mm_options)
 
         if mm_choice == 'Save/Load':
-            menu_save_load()
+            if not game_character_selected:
+                clear()
+                print('Please select a character first before saving')
+                time.sleep(2)
+            else:
+                menu_save_load()
         elif mm_choice == 'Options':
             menu_options()
+        elif mm_choice == 'Select Character':
+            player.select()
+        elif mm_choice == 'Create Character':
+            player.create(0)
+        elif mm_choice == 'Delete Character':
+            player.delete()
         else:
             game.exit()
 
